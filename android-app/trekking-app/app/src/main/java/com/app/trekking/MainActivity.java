40512b63a658081733e9b652f2b3b0cece7bb03b
dothanhlam97 <dothanhlam.98@gmail.com>
2018-05-12 13:11:34 +0700

    update map

--- android-app/trekking-app/app/src/main/java/com/app/trekking/MainActivity.java
+++ android-app/trekking-app/app/src/main/java/com/app/trekking/MainActivity.java
@@ -1,7 +1,13 @@
 package com.app.trekking;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -34,6 +40,20 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    static public final int REQUEST_LOCATION = 1;
+    static public boolean PERMISSION_ACCESS_LOCATION = true;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION) {
+            if(grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                PERMISSION_ACCESS_LOCATION = true;
+            } else {
+                PERMISSION_ACCESS_LOCATION = false;
+            }
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -42,5 +62,25 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //        get your location
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            System.out.print("Not permission.");
+            // Should we show an explanation?
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }
+        if (PERMISSION_ACCESS_LOCATION) {
+            Log.d("success", "yes");
+            mMap.setMyLocationEnabled(true);
+
+            LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+//            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+//                    LOCATION_REFRESH_DISTANCE, mLocationListener);
+        }
+
     }
 }
